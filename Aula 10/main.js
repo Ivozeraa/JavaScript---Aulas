9be9d04a45b2c1a61b22c4d64e8809d07b87da9a
Ivozera <ivozeraofc@gmail.com>
2024-01-11 21:49:56 -0300

    Loops

---
+++ Aula 10/main.js
@@ -0,0 +1,15 @@
+/*var count = 1
+while (count <= 6){ //Enquanto
+    console.log(`Passo ${count}`)
+    count++
+}
+
+do { //faça
+    while()
+}*/
+
+var count = 8
+while (count >= 1){ //Enquanto
+    console.log(`Passo ${count}`)
+    count--
+}
---
+++ Aula 11/main.js
@@ -0,0 +1,5 @@
+console.log('Vai iniciar')
+for (var c=1; c<=10; c++){
+    console.log(`Estou em passo ${c}`)
+}
+console.log('Fim')
